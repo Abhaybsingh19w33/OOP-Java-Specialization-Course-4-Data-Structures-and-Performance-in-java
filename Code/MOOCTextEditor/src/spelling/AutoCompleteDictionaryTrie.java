3f81e13495692198a266e9942a4e776a469d5a94
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-04-13 00:02:29 +0530

    Week 5 solution

--- Code/MOOCTextEditor/src/spelling/AutoCompleteDictionaryTrie.java
+++ Code/MOOCTextEditor/src/spelling/AutoCompleteDictionaryTrie.java
@@ -20,6 +20,7 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
     public AutoCompleteDictionaryTrie()
 	{
 		root = new TrieNode();
+		size = 0;
 	}
 	
 	
@@ -40,6 +41,21 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
 	public boolean addWord(String word)
 	{
 	    //TODO: Implement this method.
+		String wordToAdd = word.toLowerCase();
+		TrieNode node = root;
+		for (int i = 0; i < wordToAdd.length(); i++) {
+			char c = wordToAdd.charAt(i);
+			if (node.getValidNextCharacters().contains(c)) {
+				node = node.getChild(c);
+			} else {
+				node = node.insert(c);
+			}
+		}
+		if (!node.endsWord()) {
+			node.setEndsWord(true);
+			size++;
+			return true;
+		}
 	    return false;
 	}
 	
@@ -50,7 +66,7 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
 	public int size()
 	{
 	    //TODO: Implement this method
-	    return 0;
+	    return size;
 	}
 	
 	
@@ -60,9 +76,23 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
 	public boolean isWord(String s) 
 	{
 	    // TODO: Implement this method
+		String sToCheck = s.toLowerCase();
+		TrieNode node = root;
+		for (int i = 0; i < sToCheck.length(); i++) {
+			char c = sToCheck.charAt(i);
+			if (node.getValidNextCharacters().contains(c)) {
+				node = node.getChild(c);
+			} else {
+				return false;
+			}
+		}
+		if (node.endsWord()) {
+			return true;
+		}
 		return false;
 	}
 
+	
 	/** 
      * Return a list, in order of increasing (non-decreasing) word length,
      * containing the numCompletions shortest legal completions 
@@ -100,8 +130,44 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
     	 //       If it is a word, add it to the completions list
     	 //       Add all of its child nodes to the back of the queue
     	 // Return the list of completions
+    	 String prefixToCheck = prefix.toLowerCase();
+    	 List<String> result = new LinkedList<String>();
+    	 TrieNode node = root;
+    	 for (int i = 0; i < prefixToCheck.length(); i++) {
+    		 char c = prefixToCheck.charAt(i);
+    		 if (node.getValidNextCharacters().contains(c)) {
+ 				node = node.getChild(c);
+ 			} else {
+ 				return result;
+ 			}
+    	 }
+    	 int count = 0;
+    	 if (node.endsWord()) {
+    		 result.add(node.getText());
+    		 count++;
+    	 }
     	 
-         return null;
+    	 List<TrieNode> nodeQueue = new LinkedList<TrieNode>();
+    	 List<Character> childrenC = new LinkedList<Character>(node.getValidNextCharacters());
+    	 
+    	 for (int i = 0; i < childrenC.size(); i++) {
+    		 char c = childrenC.get(i);
+    		 nodeQueue.add(node.getChild(c));
+    	 }
+    	 while (!nodeQueue.isEmpty() && count < numCompletions) {
+    		 TrieNode tn = nodeQueue.remove(0);
+    		 if (tn.endsWord()) {
+    			 result.add(tn.getText());
+    			 count++;
+    		 }
+    		 
+    		 List<Character> cs = new LinkedList<Character>(tn.getValidNextCharacters());
+        	 for (int i = 0; i < cs.size(); i++) {
+        		 char c = cs.get(i);
+        		 nodeQueue.add(tn.getChild(c));
+        	 }
+    	 }
+         return result;
      }
 
  	// For debugging
@@ -124,7 +190,7 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
  			printNode(next);
  		}
  	}
- 	
+	 
 
 	
 }
